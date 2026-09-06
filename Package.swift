0165cf446a94696dedbacd1d8ab2ec755fc52329
imclaren <iain@firesphere.com>
2026-09-06 16:21:17 +1000

    Lower the Swift package to macOS 12
    It declared macOS 13 for no reason: nothing in it needs anything newer than 12,
    which the same code proved by compiling into an app targeting 12 when it was
    vendored there. Declaring 13 stopped that app adopting the package.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Vero",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v12)],
     products: [
         .library(name: "Vero", targets: ["Vero"]),
     ],
